617185026cfe4091b450f212c9f237d8ca331c0e
Aguskryss <aguskryss@gmail.com>
2025-09-30 23:36:48 -0300

    Fin proyecto

--- src/breakingbad/breakingBad-app.js
+++ src/breakingbad/breakingBad-app.js
@@ -3,13 +3,13 @@
  * @returns {Promise<Object>} quote information
  */
 const fetchQuote = async() => {
-
-
-    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
-    const data = await res.json();
     
+    const res = await fetch('https://www.breakingbadapi.com/api/quote/random');
+    const data = await res.json();
+
+    console.log(data[0]);
     return data[0];
-};
+}
 
 
 
@@ -17,33 +17,31 @@ const fetchQuote = async() => {
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingBadApp = async (  element  ) => {
-    
+export const BreakingbadApp = async( element ) => {
     document.querySelector('#app-title').innerHTML = 'Breakingbad App';
     element.innerHTML = 'Loading...';
-    
-    //await fetchQuote();
-    const quoteLabel = document.createElement('blackquote');
-    const authorLabel = document.createElement('h3');
+    // await fetchQuote();
+
+    const quoteLabel = document.createElement('blockquote');
+    const authoLabel = document.createElement('h3');
     const nextQuoteButton = document.createElement('button');
-    nextQuoteButton.innerText = 'Next quote';
+    nextQuoteButton.innerText = 'Next Quote';
 
 
-    const renderQuote = (quote) => {
-        quoteLabel.innerHTML = quote.quote;
-        authorLabel.innerHTML = quote.author;
-
-        element.replaceChildren(  quoteLabel, authorLabel, nextQuoteButton  );
+    const renderQuote = ( data ) => {
+        quoteLabel.innerHTML = data.quote;
+        authoLabel.innerHTML = data.author;
+        element.replaceChildren( quoteLabel, authoLabel, nextQuoteButton );
     }
 
+    // Añadir listener
     nextQuoteButton.addEventListener('click', async() => {
         element.innerHTML = 'Loading...';
         const quote = await fetchQuote();
-        renderQuote(quote);
-    });
+        renderQuote( quote );
+    })
+    
 
     fetchQuote()
-        .then(renderQuote);
-
-
-};
+        .then( renderQuote );
+}
